980c4981d5068fcb44bbc1a692adf7b3cbf8e440
carlos-witek <carlos-witek@users.noreply.github.com>
2018-06-22 16:27:42 +0100

    2018-06-22-kafka

---
+++ 2018-06-22-kafka/src/main/java/org/carlos_witek/kafka/Consumer.java
@@ -0,0 +1,9 @@
+package org.carlos_witek.kafka;
+
+public class Consumer {
+
+	public static void main( String[] args ) {
+
+	}
+
+}
---
+++ 2018-06-22-kafka/src/main/java/org/carlos_witek/kafka/Publisher.java
@@ -0,0 +1,9 @@
+package org.carlos_witek.kafka;
+
+public class Publisher {
+
+	public static void main( String[] args ) {
+
+	}
+
+}
