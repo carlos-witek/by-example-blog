74f467c7a0e3ff3c3fa64fc0697b1a6cf94ec41b
carlos-witek <carlos-witek@users.noreply.github.com>
2018-06-29 15:19:31 +0100

    tutorial code

--- 2018-06-22-kafka/src/main/java/org/carlos_witek/kafka/Consumer.java
+++
@@ -1,9 +0,0 @@
-package org.carlos_witek.kafka;
-
-public class Consumer {
-
-	public static void main( String[] args ) {
-
-	}
-
-}
--- 2018-06-22-kafka/src/main/java/org/carlos_witek/kafka/Publisher.java
+++
@@ -1,9 +0,0 @@
-package org.carlos_witek.kafka;
-
-public class Publisher {
-
-	public static void main( String[] args ) {
-
-	}
-
-}
